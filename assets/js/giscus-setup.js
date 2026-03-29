55adfb108cb72caee90e81d7110068eea470072f
wdrichard7 <85895617+wdrichard7@users.noreply.github.com>
2026-03-29 21:26:11 +0000

    Deploying to gh-pages from @ wdrichard7/wdrichard7.github.io@065fac06501e953998b226875b13ddfed95bb08a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "wdrichard7/Mr.-Richard-s-Classroom",
+    "data-repo": "wdrichard7/wdrichard7.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
